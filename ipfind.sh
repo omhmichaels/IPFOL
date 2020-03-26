8aa8560f842dcf86d383292ad4d32c21f00fe3b7
Constantine <12847926+cybersecgeek@users.noreply.github.com>
2020-03-26 06:10:15 -0700

    Add files via upload

---
+++ ipfind.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#this script only works using one(1) file at a time
+#move this script where the log file you want to analyse resides
+echo Please enter a filename [ie:mylog.txt] to look for IP addresses:
+cd logdir
+read myfilename
+echo "-------------------------------------------"
+grep -E -n -o -T "([0-9]{1,3}[\.]){3}[0-9]{1,3}" $myfilename |awk '{print "IP address " $2 $3 " found" " on line # "  $1 }' | sed 's/:/ /g'
+# -H prints filename
+# -T --initial-tab?
+echo "--------------------------------------------"
+read -p "Press [Enter] to exit..."
